22639b71cdc59d429690d90cd25beb16700488f4
Navin0305 <132676613+Navin0305@users.noreply.github.com>
2023-05-06 12:32:03 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Computer {
+	public static void accessDocuments() {
+		System.out.println("To Access Documents");
+		}
+	public static void playGames() {
+		System.out.println("Enjoying Games");
+		}
+	public static void watchMovies() {
+		System.out.println("Watching Movies");
+		}
+	public static void main(String[] args) {
+		Computer Pc=new Computer();
+		Pc.accessDocuments();
+		Pc.playGames();
+		Pc.watchMovies();
+		
+		
+	}
+
+}
